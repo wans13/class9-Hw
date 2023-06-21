2937c90fb4cad01593cf96de2543f9d91ca43bca
wans13 <130600814+wans13@users.noreply.github.com>
2023-06-20 21:29:01 -0400

    Add files via upload

---
+++ Hw.js
@@ -0,0 +1,17 @@
+const exampleArray = [
+  "The Lord of the Rings",
+  "Harry Potter and the Chamber of Secrets",
+  "The Matrix",
+  "Interstellar",
+];
+
+function abbreviate(array) {
+  array.forEach(function (movie) {
+    const separateWords = movie.split(" ");
+    const abbreviation = separateWords.map(function (word) {
+      return word[0] + ".";
+    });
+    console.log(abbreviation.join(""));
+  });
+}
+abbreviate(exampleArray);
